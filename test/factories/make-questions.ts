f6d5aff87f106762aa40d03665502fb0b80eb601
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-22 09:40:08 -0400

    feat: integrate faker for dynamic question data in makeQuestion factory

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -1,17 +1,21 @@
 import { UniqueEntityId } from "@/core/entities/unique-entity-id.js";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question.js";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug.js";
+import { faker } from '@faker-js/faker'
 
 export function makeQuestion(
-  override: Partial<QuestionProps> = {}
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
 ) {
   const question = Question.create({
     authorId: new UniqueEntityId(),
-    title: 'Example Question',
+    title: faker.lorem.sentence(),
     slug: Slug.create('example-question'),
-    content: 'Example content',
+    content: faker.lorem.text(),
     ...override
-  })
+  },
+    id
+  )
 
   return question
 } 
