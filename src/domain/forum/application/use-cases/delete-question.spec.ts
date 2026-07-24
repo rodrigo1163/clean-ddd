e85c7d2ae2cd3a0240638d542c98ff478cb8dd63
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-24 13:42:32 -0400

    feat: implement DeleteQuestionUseCase and corresponding tests, add findById and delete methods to QuestionsRepository

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryQuestionsRepository } from '../../../../../test/repositories/in-memory-questions-repository.js'
+import { makeQuestion } from '../../../../../test/factories/make-questions.js'
+import { Slug } from '../../enterprise/entities/value-objects/slug.js'
+import { DeleteQuestionUseCase } from './delete-question.js'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id.js'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityId('author-1')
+    }, new UniqueEntityId('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: 'question-1',
+      authorId: 'author-1'
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityId('author-1')
+    }, new UniqueEntityId('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(() => sut.execute({
+      questionId: 'question-1',
+      authorId: 'author-2'
+    })).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -18,4 +18,20 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
 }
