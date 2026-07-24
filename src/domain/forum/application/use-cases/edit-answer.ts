4dbfed1caf38ff8a28a3bba7cd24db482c92bea0
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-24 14:11:49 -0400

    feat: update EditAnswerUseCase and EditQuestionUseCase to return response objects with edited entities

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,9 +1,14 @@
+import { Answer } from "../../enterprise/entities/answer.js"
 import { AnswerRepository } from "../repositories/answer-repository.js"
 
 interface EditAnswerUseCaseRequest {
   authorId: string
   answerId: string
   content: string
+}
+
+interface EditAnswerUseCaseResponse {
+  answer: Answer
 }
 
 export class EditAnswerUseCase {
@@ -13,7 +18,7 @@ export class EditAnswerUseCase {
     authorId,
     content,
     answerId
-  }: EditAnswerUseCaseRequest): Promise<void> {
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
     const answer = await this.answersRepository.findById(answerId)
 
     if (!answer) {
@@ -27,5 +32,9 @@ export class EditAnswerUseCase {
     answer.content = content
 
     await this.answersRepository.save(answer)
+
+    return {
+      answer
+    }
   }
 }
--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,3 +1,4 @@
+import { Question } from "../../enterprise/entities/question.js"
 import { QuestionsRepository } from "../repositories/questions-repository.js"
 
 interface EditQuestionUseCaseRequest {
@@ -5,6 +6,10 @@ interface EditQuestionUseCaseRequest {
   questionId: string
   title: string
   content: string
+}
+
+interface EditQuestionUseCaseResponse {
+  question: Question
 }
 
 export class EditQuestionUseCase {
@@ -15,7 +20,7 @@ export class EditQuestionUseCase {
     content,
     title,
     questionId
-  }: EditQuestionUseCaseRequest): Promise<void> {
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId)
 
     if (!question) {
@@ -30,5 +35,9 @@ export class EditQuestionUseCase {
     question.content = content
 
     await this.questionsRepository.save(question)
+
+    return {
+      question
+    }
   }
 }
