bba34f83345e614c3ac30de6b2789b7c3a7f7295
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-28 16:42:56 -0400

    feat: implement AnswerComment and QuestionComment entities with properties and methods

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,55 @@
+import { Entity } from "@/core/entities/entity.js"
+import { UniqueEntityId } from "@/core/entities/unique-entity-id.js"
+import { Optional } from "@/core/types/optional.js"
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityId
+  answerId: UniqueEntityId
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+  get answerId() {
+    return this.props.answerId
+  }
+
+  get content() {
+    return this.props.content
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const answerComment = new AnswerComment({
+      ...props,
+      createdAt: props.createdAt ?? new Date()
+    }, id)
+
+    return answerComment
+  }
+}
