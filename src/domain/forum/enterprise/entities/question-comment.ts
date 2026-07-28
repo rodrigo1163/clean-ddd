e90a6b057d13d1ee0a42f4d487349b6bc45c40e3
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-28 16:56:08 -0400

    feat: refactor AnswerComment and QuestionComment to extend Comment class, removing redundant properties and methods

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,44 +1,15 @@
 import { Entity } from "@/core/entities/entity.js"
 import { UniqueEntityId } from "@/core/entities/unique-entity-id.js"
 import { Optional } from "@/core/types/optional.js"
+import { Comment, CommentProps } from "./comment.js"
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityId
-  answerId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
+export interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityId
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-  get answerId() {
-    return this.props.answerId
-  }
-
-  get content() {
-    return this.props.content
-  }
-  get createdAt() {
-    return this.props.createdAt
-  }
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  get excerpt() {
-    return this.content.substring(0, 120).trimEnd().concat('...')
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(
